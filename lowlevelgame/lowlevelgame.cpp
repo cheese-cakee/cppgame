57a6d4547f482ff23e7ddb911d435dae187655ef
cheese-cakee <farzanaman99@gmail.com>
2025-10-23 16:03:22 +0530

    Add project files.

---
+++ lowlevelgame/lowlevelgame.cpp
@@ -0,0 +1,64 @@
+﻿
+#include<SDL3/SDL.h>
+#include<SDL3/SDL_main.h>
+
+using namespace std;
+
+struct SDLState
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+};
+
+void cleanup(SDLState &state);
+
+int main(int argc,char *argv[])
+{
+	SDLState state;
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
+		return 1;
+	}
+
+	//create the window
+	int width = 800;
+	int height = 600;
+	state.window = SDL_CreateWindow("Low Level Game",width,height,0);
+
+	if(!state.window) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window",state.window);
+		cleanup(state);
+		return 1;
+	}
+
+	//start the game loop
+	bool running = true;
+	while (running)
+	{
+
+		SDL_Event event{ 0 };
+		while (SDL_PollEvent(&event)) 
+		{
+			switch (event.type)
+			{
+			case SDL_EVENT_QUIT:
+			{
+				running = false;
+				break;
+			}
+			}
+			
+		}
+		
+	}
+
+	cleanup(state);	
+	return 0;
+}
+
+void cleanup(SDLState &state)
+{
+    SDL_DestroyWindow(state.window);
+	SDL_Quit();
+}
